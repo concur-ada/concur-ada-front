e5cd1b3d3a2fbe458e614a41726406a596b855e2
Ansu John <ansu.john@sap.com>
2020-12-02 14:16:44 +0000

    having a go with fundamental react library

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,6 @@
 import React, {useRef, useEffect, useCallback} from 'react';
 import {useDispatch, useSelector} from 'react-redux';
-
+import {Shellbar} from 'fundamental-react/lib/Shellbar';
 import {ShellBar} from '@ui5/webcomponents-react/lib/ShellBar';
 import {Avatar} from '@ui5/webcomponents-react/lib/Avatar';
 import '@ui5/webcomponents-icons/dist/customer';
@@ -9,6 +9,8 @@ import '@ui5/webcomponents-icons/dist/nav-back';
 import {loginAction} from '../services/auth';
 import Login from './Login';
 import {Link} from 'react-router-dom';
+
+import './Header.css';
 
 const Header = () => {
     const refUserName = useRef();
@@ -39,6 +41,18 @@ const Header = () => {
 
     return (
         <div>
+            <Shellbar
+                className="header"
+                backAction={<Link to='/home'>
+                </Link>}
+                onProfileClick={handleProfileClick}
+                logo={<img alt='SAP' src='images/sap-concur.png' />}
+                productTitle='PRODUCTS'
+                profile={<Avatar glyph='customer'/>}
+                />
+
+
+
             <ShellBar
                 className=""
                 logo={<img slot='logo' alt='logo' src='images/sap-concur.png'/>}
