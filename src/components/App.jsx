1b44874f8259112df2c864e7cd377bdfc88fb785
LiCern <lisacernilogar@me.com>
2020-12-10 09:18:25 +0000

    Add initial task dials and days dials

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import {
-    BrowserRouter as Router,
-    Switch,
-    Route,
-    Redirect
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
 } from 'react-router-dom';
 import { Provider } from 'react-redux';
 
@@ -12,28 +12,32 @@ import Header from './Header';
 import Home from './Home';
 import DemoCounter from './DemoCounter';
 import Footer from './Footer';
+import CircleProgressBar from './CompletionDial';
+import DaysDial from './DaysDial';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 const App = () => (
-    <Provider store={store}>
-        <Router>
-            <div>
-                <Header/>
-                <Switch>
-                    <Route path='/home'>
-                        <Home/>
-                    </Route>
-                    <Route path='/demoCounter'>
-                        <DemoCounter/>
-                    </Route>
-                    <Redirect from='/' to='/home'/>
-                </Switch>
-                <Footer/>
-            </div>
-        </Router>
-    </Provider>
+  <Provider store={store}>
+    <Router>
+      <div>
+        <Header />
+        <Switch>
+          <Route path="/home">
+            <Home />
+            <CircleProgressBar percentage={75} speed={50} />
+            <DaysDial percentage={15} speed={50} />
+          </Route>
+          <Route path="/demoCounter">
+            <DemoCounter />
+          </Route>
+          <Redirect from="/" to="/home" />
+        </Switch>
+        <Footer />
+      </div>
+    </Router>
+  </Provider>
 );
 
 export default App;
---
+++ src/components/DaysDial.jsx
@@ -0,0 +1,172 @@
+import React, { useState, useEffect } from 'react';
+import { string, number, bool } from 'prop-types';
+import styled from 'styled-components';
+
+const INITIAL_OFFSET = 25;
+const circleConfig = {
+  viewBox: '0 0 38 38',
+  x: '19',
+  y: '19',
+  radio: '15.91549430918954',
+};
+
+const DaysDialBase = ({
+  className,
+  strokeColor,
+  strokeWidth,
+  innerText,
+  legendText,
+  percentage,
+  trailStrokeWidth,
+  trailStrokeColor,
+  trailSpaced,
+  speed,
+}) => {
+  const [progressBar, setProgressBar] = useState(0);
+  const pace = percentage / speed;
+  const updatePercentage = () => {
+    setTimeout(() => {
+      setProgressBar(progressBar + 1);
+    }, pace);
+  };
+
+  useEffect(() => {
+    if (percentage > 0) updatePercentage();
+  }, [percentage]);
+
+  useEffect(() => {
+    if (progressBar < percentage) updatePercentage();
+  }, [progressBar]);
+
+  return (
+    <figure className={className}>
+      <svg viewBox={circleConfig.viewBox}>
+        <circle
+          className="donut-ring"
+          cx={circleConfig.x}
+          cy={circleConfig.y}
+          r={circleConfig.radio}
+          fill="transparent"
+          stroke={trailStrokeColor}
+          strokeWidth={trailStrokeWidth}
+          strokeDasharray={trailSpaced ? 1 : 0}
+        />
+
+        <circle
+          className="donut-segment"
+          cx={circleConfig.x}
+          cy={circleConfig.y}
+          r={circleConfig.radio}
+          fill="transparent"
+          stroke={strokeColor}
+          strokeWidth={strokeWidth}
+          strokeDasharray={`${progressBar * 3.33} ${100 - progressBar * 3.33}`}
+          strokeDashoffset={INITIAL_OFFSET}
+        />
+
+        <g className="chart-text">
+          <text x="50%" y="50%" className="chart-number">
+            {progressBar}
+          </text>
+          <text x="50%" y="50%" className="chart-label">
+            {innerText}
+          </text>
+        </g>
+      </svg>
+      {legendText && (
+        <figcaption className="figure-key">
+          <ul
+            className="figure-key-list"
+            aria-hidden="true"
+            role="presentation"
+          >
+            <li>
+              <span className="shape-circle" />
+              <span>{legendText}</span>
+            </li>
+          </ul>
+        </figcaption>
+      )}
+    </figure>
+  );
+};
+
+DaysDialBase.propTypes = {
+  className: string.isRequired,
+  strokeColor: string,
+  strokeWidth: number,
+  innerText: string,
+  legendText: string,
+  percentage: number,
+  trailStrokeWidth: number,
+  trailStrokeColor: string,
+  trailSpaced: bool,
+  speed: number,
+};
+
+DaysDialBase.defaultProps = {
+  strokeColor: 'blueviolet',
+  strokeWidth: 1,
+  innerText: 'days completed',
+  legendText: '',
+  percentage: 0,
+  trailStrokeWidth: 1,
+  trailStrokeColor: '#d2d3d4',
+  trailSpaced: false,
+  speed: 1,
+};
+
+const DaysDial = styled(DaysDialBase)`
+  max-width: ${(props) => props.maxSize};
+  vertical-align: middle;
+  .chart-text {
+    fill: ${(props) => props.textColor};
+    transform: translateY(0.25em);
+  }
+  .chart-number {
+    font-size: 0.6em;
+    line-height: 1;
+    text-anchor: middle;
+    transform: translateY(-0.25em);
+  }
+  .chart-label {
+    font-size: 0.2em;
+    text-transform: uppercase;
+    text-anchor: middle;
+    transform: translateY(0.7em);
+  }
+  .figure-key [class*='shape-'] {
+    margin-right: 8px;
+  }
+  .figure-key-list {
+    list-style: none;
+    display: flex;
+    justify-content: space-between;
+  }
+  .figure-key-list li {
+    margin: 5px auto;
+  }
+  .shape-circle {
+    display: inline-block;
+    vertical-align: middle;
+    width: 21px;
+    height: 21px;
+    border-radius: 50%;
+    background-color: ${(props) => props.strokeColor};
+    text-transform: capitalize;
+  }
+`;
+
+DaysDial.propTypes = {
+  textColor: string,
+  strokeColor: string,
+  maxSize: string,
+};
+
+DaysDial.defaultProps = {
+  textColor: 'black',
+  strokeColor: 'green',
+  maxSize: '30vh',
+};
+
+export default DaysDial;
