e2b0a051310dbc34b4525bb765746f7719677e2a
Ross McMillan <73564091+rnb16178@users.noreply.github.com>
2020-12-14 17:47:30 +0000

    ammended requests

--- src/components/invite.jsx
+++ src/components/invite.jsx
@@ -1,4 +1,6 @@
 import React, { useRef, useEffect, useCallback } from 'react';
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
 const submitClick = () => {
 
 };
@@ -6,24 +8,26 @@ const submitClick = () => {
 const Invite = () => {
     return (
         <div>
+                        <Container>
+
             <p>Dear [name]</p>
             <p>Welcome to SAP Concur</p>
             <p>Your Finance business transformation begins here. You have purchased the following services from us:</p>
             <p>In order to move to the next stage of our journey together we now need to ask for some basic information. If at any point you have any questions please feel free to contact your sales person [name] or implementation.</p>
             <p>1. Please confirm your contact details and company roll</p>
             <tr>
-                <td>Name:</td>
+                <td><p>Name</p></td>
                 <td><input type="text" placeholder="name" required></input></td>
             </tr>
             <tr>
-                <td>job title:</td>
+                <td><p>job title:</p></td>
                 <td><input type="text" placeholder="job title" required></input></td>
                 </tr>
             <tr>
-                <td>Choose a role from this list or enter your own:</td>
+                <td><p>Choose a role from this list or enter your own:</p></td>
                 <td>
                     <label>
-                <input list="roles" name="roleList" required></input>
+                <input list="roles" name="roleList" placeholder="role" required></input>
                 </label>
                 <datalist id="roles">
                     <option value="Executive Sponsor"></option>
@@ -31,9 +35,9 @@ const Invite = () => {
                     <option value="Finance Manager "></option>
                 </datalist></td>
                 </tr>
-            <tr><td>GDPR </td><td><input type="checkbox" id="GDPR" name="GDPR" value="GDPR" required></input></td></tr>
-            <button onClick={submitClick}>Next</button>
-
+            <tr><td><p>GDPR</p> </td><td><input type="checkbox" id="GDPR" name="GDPR" value="GDPR" required></input></td></tr>
+            <Button onClick={submitClick}>Next</Button>
+</Container>
         </div>
 
     );
