ea91258ddb3fdf4d41d668a2cd4a1a283a094daf
Valli Ramaswamy <Vallikkannu.Ramaswamy@city.ac.uk>
2020-12-16 11:25:16 +0000

    adding links to name bar

--- src/components/NameBadgeDisplay.jsx
+++ src/components/NameBadgeDisplay.jsx
@@ -4,6 +4,7 @@ import { ThemeProvider } from '@ui5/webcomponents-react/lib/ThemeProvider';
 import { Bar } from '@ui5/webcomponents-react/lib/Bar';
 import '@ui5/webcomponents-icons/dist/badge';
 import '@ui5/webcomponents/dist/Icon.js';
+import { Link } from '@ui5/webcomponents-react/lib/Link';
 
 const NameBadgeDisplay = () => {
 
@@ -17,6 +18,7 @@ const NameBadgeDisplay = () => {
         <Bar
           className='nameBadgeHeader'
           contentLeft={<> {name}</>}
+          contentMiddle={<><Link  href='/learn'>LEARN</Link> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<Link  target='_blank' href='https://www.concur.com/en-us/events'>EVENTS</Link> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<Link target='_blank' href='https://www.concur.com/en-us/free-trial'>PURCHASE</Link> </>}
           contentRight={<>{badges}<ui5-icon className="badgeIcon" style={{color:'#f0ab00', width:'30px', position:'relative',bottom:'5px'}}name="badge"></ui5-icon></>}
           design='Header'
           style={{backgroundColor:'black', height:'50px'}}
