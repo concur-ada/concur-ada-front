a30f631cb93dd7404260b8d6dd2b5b63a9eccbad
Ross McMillan <73564091+rnb16178@users.noreply.github.com>
2020-12-16 16:38:01 +0000

    hardcoded invite journey

--- src/components/invite.jsx
+++ src/components/invite.jsx
@@ -1,32 +1,36 @@
 import React from 'react';
 import Button from 'react-bootstrap/Button';
 import Container from 'react-bootstrap/Container';
-import {useSelector} from 'react-redux';
-
+import { useSelector } from 'react-redux';
+import {Redirect} from 'react-router-dom';
+import {useHistory } from 'react-router-dom';
 
 
 const Invite = () => {
-    const {user} = useSelector(state => state.user);
+    const history = useHistory();
+    const { user } = useSelector(state => state.user);
+    let name = user && user.name;
+    let role = user && user.role;
     const submitClick = () => {
-    let role = document.getElementById('selectedRole').value;
-    let name = document.getElementById('name').value;
-    let job = document.getElementById('jobTitle').value;
-    let GDPR=document.getElementById('GDPR').checked;
-    
-if(GDPR){
+        let GDPR = document.getElementById('GDPR').checked;
 
-}else{
-    alert('Please accept GDPR before continuing');
-}
-    console.log(role+name+job+GDPR);
-    //save to redux
-    
+        if (GDPR) {
+            //save to redux here
+            history.push('/landing');
 
-};
+        } else {
+            alert('Please accept GDPR before continuing');
+        }
+    };
+    const onTextChange = (event)=>{
+        name=event.target.value;
+    };
     return (
+        <>
+            {user ? null : <Redirect to="/login"/>}
         <div>
             <Container>
-                <p>Dear {user}</p>
+                <p>Dear {name}</p>
                 <p>Welcome to SAP Concur</p>
                 <p>Your Finance business transformation journey begins here. You have purchased the following services from us:</p>
                 <ul>
@@ -36,20 +40,20 @@ if(GDPR){
                 </ul>
                 <p>In order to move to the next stage of our journey together we now need to ask for some basic information. If at any point you have any questions please feel free to contact your sales person [name] or implementation.</p>
                 <p>Please confirm your contact details & company role</p>
-                
+
                 <tr>
                     <td><p>Name</p></td>
-                    <td><input id="name" type="text" placeholder="Enter Name" required></input></td>
+                    <td><input id="name" type="text" placeholder="Enter Name" value ={name} onChange={onTextChange} required></input></td>
                 </tr>
                 <tr>
                     <td><p>Job Title:</p></td>
-                    <td><input id="jobTitle" type="text" placeholder="Enter Job Title" required></input></td>
+                    <td><input id="jobTitle" type="text" placeholder="Enter Job Title" value ={role} required></input></td>
                 </tr>
                 <tr>
                     <td><p>Choose a role from this list or enter your own:</p></td>
                     <td>
                         <label>
-                            <input id="selectedRole"list="roles" name="roleList" placeholder="Enter or Select Role" required></input>
+                            <input id="selectedRole" list="roles" name="roleList" placeholder="Enter or Select Role" required></input>
                         </label>
                         <datalist id="roles">
                             <option value="Executive Sponsor"></option>
@@ -59,11 +63,12 @@ if(GDPR){
                 </tr>
                 <tr>
                     <td><p>GDPR</p> </td>
-                    <td><input type="checkbox" id="GDPR" name="GDPR"  required></input></td>
+                    <td><input type="checkbox" id="GDPR" name="GDPR" required></input></td>
                 </tr>
                 <Button onClick={submitClick}>Save & Proceed</Button>
             </Container>
         </div>
+        </>
 
     );
 };
